d2079f4c4a38f7789abc8a3e128b1fc23401dc28
Emil Sjunnesson <byrhult@gmail.com>
2014-11-25 18:23:31 +0100

    Better looking test

--- webroot/test_rss.php
+++ webroot/test_rss.php
@@ -4,5 +4,10 @@ require '../src/library/CRSS.php';
 $feed = new \Emsf14\RSS\CRSS([
 	'http://dailycatdrawings.tumblr.com/rss'
 ]);
+?>
 
-echo $feed->printFeed();
+<!doctype html>
+<meta charset=utf8>
+<title>RSS Example</title>
+<h1>RSS Example</h1>
+<?=$feed->printFeed()?>
