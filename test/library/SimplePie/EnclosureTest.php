116d785b642ac5eabc333857fe14283a1c5a334d
Emil Sjunnesson <byrhult@gmail.com>
2014-11-27 16:44:59 +0100

    test enclosure

---
+++ test/library/SimplePie/EnclosureTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class SimplePie_EnclosureTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test
+     *
+     * @return void
+     *
+     */
+    public function testprintFeed()
+    {
+        $enclosure = new \Enclosure();
+
+        $res = $encloseure->native_embed(['widescreen => true']);
+        $this->assertInternalType('string', $res, "Return type in not a string");
+    }
